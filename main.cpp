0890615ee6c6bc64adb5049a6f10028dd0e96724
Bart Durak <projektator@gmail.com>
2023-02-18 19:30:48 +0100

    do while

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-// do Kwhile nie ma iteracji
+// pętla while nie ma iteracji
 int uczniowie = 1; int minut=0;
 
 int main() {
 
-
-        do
-        {
-            minut++;
+    while (uczniowie <=10)
+    {
+        minut = minut + 4;
         uczniowie = uczniowie *2;
         cout << " minęło minut: " << minut;
         cout << " zapytai uczniowie: " << uczniowie<< endl;
+
     }
-        while (uczniowie<<=10);
 return 0;
 }
